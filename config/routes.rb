eab6cc04e850a9197b40da6ca4b50717a853280f
Per Sandström <per@helloper.com>
2016-05-17 11:15:02 +0200

    Rename confessions stuff to mirrors

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'dev-confessions' => 'pages#dev_confessions'
+  get 'dev-confessions' => 'pages#dev_mirrors'
 
   resources :confessions, only: [:index, :new, :edit, :create, :update, :destroy]
 
