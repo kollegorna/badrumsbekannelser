393bf99b5b7e4d4c45261b5f6ab6669faf428b5b
Per Sandström <per@helloper.com>
2016-05-17 15:51:00 +0200

    Fix mirror JS

--- app/assets/javascripts/mirrors.js
+++ app/assets/javascripts/mirrors.js
@@ -13,8 +13,8 @@ $(document).ready(function() {
    var j = 0;
    var delay = 2000; //millisecond delay between cycles
    function cycleThru(){
-           var jmax = $(".confessions p").length -1;
-           $(".confessions p:eq(" + j + ")")
+           var jmax = $(".mirror p").length -1;
+           $(".mirror p:eq(" + j + ")")
                    .animate({"opacity" : "1"} ,400)
                    .animate({"opacity" : "1"}, delay)
                    .show().animate({"opacity" : "0"}, 400, function(){
