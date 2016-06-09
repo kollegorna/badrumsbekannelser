658d68ef0ba75abcca89bb0b2e76e90a5f8bf79f
Eduardo Nunes <ed@eduardonunes.me>
2016-06-09 14:53:21 +0100

    Add countdown to edit confessions button

--- app/assets/javascripts/app/init.js
+++ app/assets/javascripts/app/init.js
@@ -1,3 +1,24 @@
 $(function () {
+  $(document).ready(function () {
 
+    if($('.button--confession-edit').length) {
+      $('.button--confession-edit').each(function() {
+        var el = $(this);
+        var time = el.attr('data-remaining-time');
+
+        var percentage = 100 - time * 100 / 1800;
+
+        el.find('.timer').css('width', percentage + '%');
+        el.find('.info time').html(Math.round(time/60));
+
+        setInterval(function() {
+          console.log(time);
+          time -= 5;
+          percentage = 100 - time * 100 / 1800;
+          el.find('.timer').animate({'width': percentage + '%'});
+          el.find('.info time').html(Math.round(time/60));
+        }, 5000)
+      });
+    };
+  });
 });
