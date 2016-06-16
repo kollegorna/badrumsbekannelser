7b63c0a51866d1a32762dba8021f31e89986bfa3
Eduardo Nunes <ed@eduardonunes.me>
2016-06-16 11:04:34 +0100

    Add all confessions page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     @cells = Cell.order(position: :asc)
   end
+
+  def all_confessions
+    @confessions = Confession.all.order(created_at: :desc)
+  end
 end
