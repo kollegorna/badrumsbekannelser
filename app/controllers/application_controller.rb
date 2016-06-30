d3ee6d94f8b5aeb0335bf5d6412a006baff40a97
Samuel Carlsson <samuel@kollegorna.se>
2016-06-30 18:02:57 +0900

    Add basic auth as a before action. Skip it on mirrors show action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :layout_by_resource
 
-  if Rails.env.production? && ENV['BASIC_AUTH']
-    http_basic_authenticate_with(
-      name: ENV['BASIC_AUTH_USERNAME'],
-      password: ENV['BASIC_AUTH_PASSWORD']
-    )
-  end
+  before_action :basic_auth if ENV['BASIC_AUTH'].eql? 'enabled'
 
   protected
 
@@ -23,6 +18,12 @@ class ApplicationController < ActionController::Base
       admin_dashboard_path
     when User
       confessions_path
+    end
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
