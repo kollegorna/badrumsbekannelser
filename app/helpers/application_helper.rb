49a4ae2b2bc4e72d4ab8ecbe93118bd9830b0870
Raymall Pérez <raymall.perez@gmail.com>
2016-08-23 02:24:21 -0400

    Fixed word line break

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,22 @@ module ApplicationHelper
 
     return wrapped_text.join
   end
+
+#  def wrap_in_span(text)
+#    text = EmojiParser.detokenize(text.gsub(EmojiParser.emoticon_regex) {|match| ":#{EmojiParser.emoticons[match]}:" })
+#
+#    wrapped_text = []
+#    wrapped_word = []
+#
+#    text.each_char do |c|
+#      if c.match(/\s+/)
+#        wrapped_text << "<span class='word'>#{wrapped_word.join}</span> "
+#        wrapped_word = []
+#      else
+#        wrapped_word << "<span class='letter'>#{c}</span>"
+#      end
+#    end
+#
+#    return wrapped_text.join
+#  end
 end
