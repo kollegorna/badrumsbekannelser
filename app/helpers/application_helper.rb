dcd2c419e9815b8f3b097b42640112fdb7654ba0
Raymall Pérez <raymall.perez@gmail.com>
2016-08-23 02:45:45 -0400

    Wrap word

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,33 +7,33 @@ module ApplicationHelper
     content_for(:description) { page_description }
   end
 
-  def wrap_in_span(text)
-    text = EmojiParser.detokenize(text.gsub(EmojiParser.emoticon_regex) {|match| ":#{EmojiParser.emoticons[match]}:" })
-
-    wrapped_text = []
-
-    text.each_char do |c|
-      wrapped_text << "<span>#{c}</span>"
-    end
-
-    return wrapped_text.join
-  end
-
 #  def wrap_in_span(text)
 #    text = EmojiParser.detokenize(text.gsub(EmojiParser.emoticon_regex) {|match| ":#{EmojiParser.emoticons[match]}:" })
 #
 #    wrapped_text = []
-#    wrapped_word = []
 #
 #    text.each_char do |c|
-#      if c.match(/\s+/)
-#        wrapped_text << "<span class='word'>#{wrapped_word.join}</span> "
-#        wrapped_word = []
-#      else
-#        wrapped_word << "<span class='letter'>#{c}</span>"
-#      end
+#      wrapped_text << "<span>#{c}</span>"
 #    end
 #
 #    return wrapped_text.join
 #  end
+
+  def wrap_in_span(text)
+    text = EmojiParser.detokenize(text.gsub(EmojiParser.emoticon_regex) {|match| ":#{EmojiParser.emoticons[match]}:" })
+
+    wrapped_text = []
+    wrapped_word = []
+
+    text.each_char do |c|
+      if c.match(/\s+/)
+        wrapped_text << "<span class='word'>#{wrapped_word.join}</span> "
+        wrapped_word = []
+      else
+        wrapped_word << "<span class='letter'>#{c}</span>"
+      end
+    end
+
+    return wrapped_text.join
+  end
 end
